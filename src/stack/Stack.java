c4bde9d44274e6bb47765c77f0d07c21233e2bcf
zekrom-vale <sgraven66@gmail.com>
2019-10-03 20:19:32 -0500

    Added comments to Stack

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -2,25 +2,58 @@ package stack;
 
 import linkedList.Node;
 
+/**
+ * Stack/LIFO/FILO Data Structure<br>
+ * Defines the Stack Data Structure and implements it with a linked list node
+ *
+ * @author     Shawn Graven (Zekrom)
+ *
+ * @param  <E>
+ *                 The type of the stack
+ */
 public class Stack <E>{
+	/**
+	 * Always false as it has no limit
+	 * @return {@code false}
+	 */
+	public static boolean isFull(){
+		return false;
+	}
+
+
+	/**
+	 * The root node that keeps the data attached to the stack
+	 */
 	Node<E> node=null;
 
 
+	/**
+	 * Creates a new stack
+	 */
 	public Stack(){}
 
+	/**
+	 * @return If the stack is empty return {@code true} else {@code false}
+	 */
 	public boolean isEmpty() {
 		return this.node==null;
 	}
 
-	public boolean isFull(){
-		return false;
-	}
-
+	/**
+	 * Returns the top element without removing it
+	 *
+	 * @return The top most element
+	 */
 	public E peek(){
 		if(this.node==null) return null;
 		return this.node.getValue();
 	}
 
+	/**
+	 * Removes the top element and returns it
+	 *
+	 * @return The removed element
+	 */
 	public E pop(){
 		if(this.node==null) return null;
 		final E value=this.node.getValue();
@@ -29,7 +62,27 @@ public class Stack <E>{
 		return value;
 	}
 
+	/**
+	 * Adds the given value to the stack
+	 *
+	 * @param value
+	 *                  The value to add
+	 */
 	public void push(final E value){
 		this.node=new Node<>(value, this.node);
+	}
+
+	/**
+	 * Adds the given values to the stack<br>
+	 * Puts the first elements in first than the last ones, making the last one the top of the stack
+	 *
+	 * @param values
+	 *                   The values to add
+	 */
+	@SuppressWarnings("unchecked")
+	public void push(final E... values){
+		for(int i=0; i<values.length; i++){
+			this.push(values[i]);
+		}
 	}
 }
