1a9d1d4f7965d4136eb57e7f341baa8cf132bb2f
zekrom-vale <sgraven66@gmail.com>
2019-10-17 13:12:00 -0500

    Comment update

--- src/problem2/QueueDDriver.java
+++ src/problem2/QueueDDriver.java
@@ -23,7 +23,7 @@ public class QueueDDriver{
 		final QueueD<Integer> queueD=new QueueD<>(10, true);
 		System.out.println(queueD.push(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
 		queueD.print();
-		//Empty out the Queue
+		//Empty out the Queue until it's empty
 		while(!queueD.isEmpty()){
 			System.out.println(queueD.pop());
 			queueD.print();
