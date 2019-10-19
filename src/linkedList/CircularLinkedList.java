b71c5822b76df88b76ee1d68a9ebfdc2f0e0aaea
Shawn <sgraven66@gmail.com>
2019-10-19 13:59:39 -0500

    Create CircularLinkedList.java

---
+++ src/linkedList/CircularLinkedList.java
@@ -0,0 +1,27 @@
+package linkedList;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+
+/**
+ * LinkedList Class<br>
+ * Defines a double linked list
+ *
+ * @author     Zekrom
+ *
+ * @param  <E>
+ *                 The type of the Linked list
+ */
+public class CircularLinkedList<@Nullable E> extends LinkedList <E>{
+	/**
+	 * Returns a new CircularLinkedList
+	 */
+	public CircularLinkedList(){
+		//Do nothing
+	}
+}
