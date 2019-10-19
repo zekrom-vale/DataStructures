40b5a5565b17a1d0ddb0bf43e18d15839f6cc48e
zekrom-vale <sgraven66@gmail.com>
2019-10-18 19:27:07 -0500

    Changed, simplifyed, and added methods for linked list

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,34 +1,70 @@
 package linkedList;
 
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 
 
+/**
+ * LinkedList Class<br>
+ * Defines a double linked list
+ *
+ * @author     Zekrom
+ *
+ * @param  <E>
+ *                 The type of the Linked list
+ */
 public class LinkedList <@Nullable
 E>{
 	@Nullable
 	private Node<E> left=null;
 	@Nullable
 	private Node<E> right=null;
+	private long size=0;
 
-
+	/**
+	 * Returns a new linked list
+	 */
 	public LinkedList(){
 		//Do nothing
 	}
 
-	public E deleteLeft(final int index){
+	/**
+	 * Removes the index from the left
+	 *
+	 * @param  index
+	 *                   The index to remove
+	 * @return       The removed value
+	 */
+	public E deleteLeft(final long index){
 		if(index==0) return this.removeLeft();
+		if(index==this.size-1) this.removeRight();
 
-		Node<E> prev=this.left;
-		int i=0;
-		while(prev!=null&&i<index-1){
-			prev=prev.getNext();
-			i++;
-		}
-		if(prev==null) return null;
+		final Node<E> prev=this.getNodeLeft(index-1);
+		if(prev==null)return null;
+
+		final Node<E> node=prev.getNext(), next=node.getNext();
+		prev.setNext(next);
+		if(next!=null) return this.removeRight();
+		return node.getValue();
+	}
+
+	/**
+	 * Removes the index from the right
+	 *
+	 * @param  index
+	 *                   The index to remove
+	 * @return       The removed value
+	 */
+	public E deleteRight(final long index){
+		if(index==0) return this.removeRight();
+		if(index==this.size-1) this.removeLeft();
+
+		final Node<E> prev=this.getNodeRight(index+1);
+		if(prev==null)return null;
 
 		final Node<E> node=prev.getNext(), next=node.getNext();
 		prev.setNext(next);
@@ -73,6 +109,7 @@ E>{
 		}
 	}
 
+
 	/**
 	 * Loops through all elements and replace the value
 	 * @param op The method to execute on each value
@@ -85,21 +122,75 @@ E>{
 		}
 	}
 
+	/**
+	 * Returns the value at the given index
+	 *
+	 * @param  index
+	 *                   The index to get
+	 * @return       The value at the index
+	 */
 	public E getLeft(final int index){
+		return this.getNodeLeft(index).getValue();
+	}
 
-		Node<E> node=this.left;
+
+	/**
+	 * Returns the node of the index from the left
+	 *
+	 * @param  index
+	 *                   The index to get the node
+	 * @return       The node of the index
+	 */
+
+	@SuppressWarnings("null")
+	protected Node<E> getNodeLeft(final long index){
+		if(index>=this.size) return null;
+		if(index>this.size/2) return this.getNodeRight(this.size-index-1);
+		@NonNull
+		Node<E> prev=this.left;
 		int i=0;
-		while(node!=null&&i<index){
-			node=node.getNext();
+		while(i<index){
+			prev=prev.getNext();
 			i++;
 		}
-		if(node==null) return null;
-		return node.getValue();
+		return prev;
+	}
+
+	/**
+	 * Returns the node of the index from the right
+	 *
+	 * @param  index
+	 *                   The index to get the node
+	 * @return       The node of the index
+	 */
+
+	@SuppressWarnings("null")
+	protected Node<E> getNodeRight(final long index){
+		if(index>=this.size) return null;
+		if(index>this.size/2) return this.getNodeRight(this.size-index-1);
+		@NonNull
+		Node<E> next=this.right;
+		int i=0;
+		while(i<index){
+			next=next.getPrevious();
+			i++;
+		}
+		return next;
+	}
+
+	/**
+	 * Returns the value at the given index
+	 * @param index The index to get
+	 * @return The value at the index
+	 */
+	public E getRight(final int index){
+		return this.getNodeRight(index).getValue();
 	}
 
 	private void insert(final E value){
 		this.left=new Node<>(value);
 		this.right=this.left;
+		this.size++;
 	}
 
 	/**
@@ -114,31 +205,39 @@ E>{
 			return;
 		}
 		this.left=new Node<>(this.left, value);
+		this.size++;
 	}
 
+	/**
+	 * Inserts the value after the left based index
+	 *
+	 * @param  index
+	 *                   The index to insert the value (Inserts after)
+	 * @param  value
+	 *                   The value to insert
+	 * @return       Success of insertion
+	 */
 	@SuppressWarnings("unused")
-	public boolean insertLeftAfter(final int index, final E value){
+	public boolean insertLeft(final int index, final E value){
 
 		if(index==-1){
 			this.insertLeft(value);
 			return true;
 		}
 
-		Node<E> prev=this.left;
-		int i=0;
-		while(prev!=null&&i<index){
-			prev=prev.getNext();
-			i++;
-		}
+		final Node<E> prev=this.getNodeLeft(index);
 		if(prev==null) return false;
 		final Node<E> next=prev.getNext();
 		if(next==null){
 			this.right=new Node<>(prev, value);
+			this.size++;
 			return true;
 		}
 		new Node<>(prev, value, next);
+		this.size++;
 		return true;
 	}
+
 
 	/**
 	 * Inserts the value at the end
@@ -149,9 +248,38 @@ E>{
 	public void insertRight(final E value){
 		if(this.left==null){
 			this.insert(value);
+			this.size++;
 			return;
 		}
 		this.right=new Node<>(value, this.right);
+		this.size++;
+	}
+
+	/**
+	 * Inserts the value after the right based index
+	 * @param index The index to insert the value (Inserts after)
+	 * @param value The value to insert
+	 * @return Success of insertion
+	 */
+	@SuppressWarnings("unused")
+	public boolean insertRight(final int index, final E value){
+
+		if(index==-1){
+			this.insertRight(value);
+			return true;
+		}
+
+		final Node<E> next=this.getNodeRight(index);
+		if(next==null) return false;
+		final Node<E> prev=next.getPrevious();
+		if(prev==null){
+			this.right=new Node<>(value, next);
+			this.size++;
+			return true;
+		}
+		new Node<>(prev, value, next);
+		this.size++;
+		return true;
 	}
 
 
@@ -167,9 +295,11 @@ E>{
 		this.left=this.left.getNext();
 		if(this.left==null){
 			this.right=null;
+			this.size--;
 			return value;
 		}
 		this.left.setPrevious(null);
+		this.size--;
 		return value;
 	}
 
@@ -186,18 +316,27 @@ E>{
 		this.right=this.right.getPrevious();
 		if(this.right==null){
 			this.right=null;
+			this.size--;
 			return value;
 		}
 		this.right.setNext(null);
+		this.size--;
 		return value;
 	}
 
-	public E searchLeft(final @NonNull
-		E value){
+	/**
+	 * Finds the value in the Linked List from the left
+	 *
+	 * @param  function
+	 *                      The function to test the value
+	 *
+	 * @return          The value matching according to the function
+	 */
+	public E searchLeft(final Predicate<E> function){
 
 		Node<E> node=this.left;
 		while(node!=null){
-			if(value.equals(node.getValue()));
+			if(function.equals(node.getValue())) return node.getValue();
 			node=node.getNext();
 		}
 		return null;
@@ -207,9 +346,11 @@ E>{
 	/**
 	 * Returns the string representation of the LinkedList
 	 */
+	@SuppressWarnings("null")
 	@Override
 	public String toString(){
-		if(this.left==null) return "LinkedList[]";
+		if(this.size==0) return "LinkedList[]";
+		@NonNull
 		Node<E> node=this.left;
 		final StringBuilder builder=new StringBuilder();
 		builder.append("LinkedList[");
