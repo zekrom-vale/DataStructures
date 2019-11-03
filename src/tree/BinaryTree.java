868c1b22d4998ad185867c49d0fb50883072f12c
Shawn <sgraven66@gmail.com>
2019-11-03 12:34:21 -0600

    Create BinaryTree.java

---
+++ src/tree/BinaryTree.java
@@ -0,0 +1,15 @@
+/**
+* Class BinaryTree defines a BinaryTree used for searching
+*/
+public class BinaryTree<@Nonnull E extends Comparable<E>>{
+	
+	Node<E> root=null;
+	
+	/**
+	* Default constructor<br>
+	* Creates a new BinaryTree
+	*/
+	public BinaryTree(){
+		//Do nothing
+	}
+}
