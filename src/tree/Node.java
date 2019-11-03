2476648576418813567971bc987a5ced262d565e
Shawn <sgraven66@gmail.com>
2019-11-03 14:11:39 -0600

    Remove should now work

--- src/tree/Node.java
+++ src/tree/Node.java
@@ -27,43 +27,49 @@ public class Node<@Nonnull E extends Comparable<E>>{
 		this(null, value, right);
 	}
 	
-	protected void insert(@Nonnull Node<E> root, E value){
-		if(this.root.value.compareTo(value)<0){
-			if(this.root.left==null){
-				this.root.left=new Node(value);
+	protected void insert(E value){
+		if(this.value.compareTo(value)<0){
+			if(this.left==null){
+				this.left=new Node(value);
 				return;
 			}
-			this.insert(root.left, value);
+			this.left.insert(value);
 			return;
 		}
-		if(this.root.right==null){
-			this.root.right=new Node(value);
+		if(this.right==null){
+			this.right=new Node(value);
 			return;
 		}
-		this.insert(root.right, value);
+		this.right.insert(value);
 	}
 	
-	protected boolean remove(@Nonnull Node<E> root, E value){
-		if(this.root.value.compareTo(value)<0){
-			if(this.root.left==null)return false;
-			if(this.root.left.value.compareTo(value)==0){
-				this.remove(root.left, root);
+	protected boolean remove(E value){
+		if(this.value.compareTo(value)<0){
+			if(this.left==null)return false;
+			if(this.left.value.compareTo(value)==0){
+				this.left.removeLeft(this);
 				return true;
 			}
-			return this.remove(root.left, value);
+			return this.left.remove(value);
 		}
 		else{
-			if(this.root.right==null)return false;
-			if(this.root.right.value.compareTo(value)==0){
-				this.remove(root.right, root);
+			if(this.right==null)return false;
+			if(this.right.value.compareTo(value)==0){
+				this.right.removeRight(this);
 				return true;
 			}
-			return this.remove(root.right, value);
+			return this.right.remove(value);
 		}
 	}
 	
-	protected void remove(@Nonnull Node<E> node, @Nonnull Node<E> parent){
-		
+	protected void removeRight(@Nonnull Node<E> parent){
+		parent.right=this.right;
+		this.leftMost().left=this.left;
+	}
+	
+	protected void removeLeft(@Nonnull Node<E> parent){
+		parent.left=this.right;
+		this.leftMost().left=this.left;
 	}
 	
 	public Node<E> leftMost(){
