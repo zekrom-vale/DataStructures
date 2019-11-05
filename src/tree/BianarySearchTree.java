07a7d293aa369d3be818ddca61aa245bf4e4caa8
zekrom-vale <sgraven66@gmail.com>
2019-11-05 08:45:20 -0600

    Delegated methods

--- src/tree/BianarySearchTree.java
+++ src/tree/BianarySearchTree.java
@@ -3,65 +3,221 @@ package tree;
 import java.util.function.Consumer;
 
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
+/**
+ * Class BianarySearchTree
+ *
+ * @author     Zekrom
+ *
+ * @param  <E>
+ *                 The type of the BianarySearchTree
+ */
 public class BianarySearchTree <@NonNull
 E extends Comparable<E>>{
+	/**
+	 * Loops through the BianarySearchTree in order
+	 * @param node the root node of the current tree or sub-tree
+	 * @param consumer The function to perform on each node
+	 */
+	private static <@NonNull E extends Comparable<E>>void forEach(final Node<E> node, final Consumer<E> consumer){
+		if(node==null) return;
+		BianarySearchTree.forEach(node.getLeft(), consumer);
+		consumer.accept(node.getValue());
+		BianarySearchTree.forEach(node.getRight(), consumer);
+	}
+
+	/**
+	 * Loops through the BianarySearchTree
+	 * @param node the root node of the current tree or sub-tree
+	 * @param consumer The function to perform on each node
+	 */
+	private  static <@NonNull E extends Comparable<E>>void forEachPost(final Node<E> node, final Consumer<E> consumer){
+		if(node==null) return;
+		BianarySearchTree.forEachPost(node.getLeft(), consumer);
+		BianarySearchTree.forEachPost(node.getRight(), consumer);
+		consumer.accept(node.getValue());
+	}
+
+	/**
+	 * Loops through the BianarySearchTree
+	 * @param node the root node of the current tree or sub-tree
+	 * @param consumer The function to perform on each node
+	 */
+	private  static <@NonNull E extends Comparable<E>>void forEachPre(final Node<E> node, final Consumer<E> consumer){
+
+		if(node==null)return;
+		consumer.accept(node.getValue());
+		BianarySearchTree.forEachPre(node.getLeft(), consumer);
+		BianarySearchTree.forEachPre(node.getRight(), consumer);
+	}
+
+
+	/**
+	 * Tests if the node is a valid BianarySearchTree
+	 *
+	 * @param  <E>
+	 *                  The type of the node
+	 * @param  root
+	 *                  The current root of the tree or sub-tree
+	 * @return      {@code true} if valid, {@code false} if invalid
+	 */
 	private static <@NonNull
-	E extends Comparable<E>> boolean
-	isBianarySearchTree(final Node<E> root){
-		return BianarySearchTree.isMin(
-			root.getLeft(), root.getValue()
-			)&&BianarySearchTree.isMax(
-				root.getRight(), root.getValue()
-				)&&BianarySearchTree.isBianarySearchTree(root.getRight())&&
-			BianarySearchTree.isBianarySearchTree(root.getLeft());
+	E extends Comparable<E>> boolean isBianarySearchTree(final @Nullable Node<E> root){
+		if(root==null)return true;
+
+		//Check to see if the sub trees are valid BianarySearchTrees
+		if(
+			!BianarySearchTree.isBianarySearchTree(root.getRight())
+			) return false;
+		if(
+			BianarySearchTree.isBianarySearchTree(root.getLeft())
+			) return false;
+
+
+		//We know that the sub trees are valid BianarySearchTrees
+		if(
+			root.left!=null&&root.compareTo(root.getLeft())<=0
+			) return false;
+		if(root.right!=null&&root.compareTo(root.getRight())>0
+			) return false;
+		return true;
 	}
 
 	private static <@NonNull
 	E extends Comparable<E>> boolean isMax(final Node<E> root, final E value){
 		if(root==null) return true;
-		return root.getValue().compareTo(value)>=0&&BianarySearchTree
-			.isMax(root.getLeft(), value)&&
-			BianarySearchTree.isMax(root.getRight(), value);
+		if(root.getValue().compareTo(value)<0) return false;
+		if(!BianarySearchTree.isMax(root.getLeft(), value)) return false;
+		return BianarySearchTree.isMax(root.getRight(), value);
 	}
+
 
 	private static <@NonNull
 	E extends Comparable<E>> boolean
 	isMin(final Node<E> root, final E value){
 		if(root==null) return true;
-		return root.getValue().compareTo(value)<0&&BianarySearchTree
-			.isMin(root.getLeft(), value)&&
-			BianarySearchTree.isMin(root.getRight(), value);
+		if(root.getValue().compareTo(value)>=0) return false;
+		if(!BianarySearchTree.isMin(root.getLeft(), value)
+			) return false;
+		return BianarySearchTree.isMin(root.getRight(), value);
 	}
 
-	private final Node<E> root=null;
+	@Nullable
+	private Node<E> root=null;
 
 
+	/**
+	 * Default Constructor
+	 *
+	 */
+	public BianarySearchTree(){
+		//Do nothing
+	}
+
+	/**
+	 * Counts the amount of leaves in the tree<br>
+	 * The nodes that do not have children
+	 *
+	 * @return the number of leaves
+	 * @see    tree.Node#countLeaves()
+	 */
+	@SuppressWarnings("null")
+	public long countLeaves(){
+		if(this.root==null)return 0;
+		return this.root.countLeaves();
+	}
+
+	/**
+	 * Counts the amount of internal nodes in the tree<br>
+	 * The nodes that do have children
+	 *
+	 * @return the number of internal nodes
+	 * @see    tree.Node#countNodes()
+	 */
+	@SuppressWarnings("null")
+	public long countNodes(){
+		if(this.root==null)return 0;
+		return this.root.countNodes();
+	}
+
+	/**
+	 * Loops through the BianarySearchTree in order
+	 * @param consumer The function to perform on each node
+	 */
 	public void forEach(final Consumer<E> consumer){
-		this.forEach(this.root, consumer);
+		BianarySearchTree.forEach(this.root, consumer);
 	}
 
-	private void forEach(final Node<E> node, final Consumer<E> consumer){
-		if(node==null) return;
-		this.forEach(node.getLeft(), consumer);
-		consumer.accept(node.getValue());
-		this.forEach(node.getRight(), consumer);
+
+	/**
+	 * Loops through the BianarySearchTree
+	 * @param consumer The function to perform on each node
+	 */
+	public void forEachPost(final Consumer<E> consumer){
+		BianarySearchTree.forEachPost(this.root, consumer);
 	}
 
-	private void forEachPost(final Node<E> node, final Consumer<E> consumer){
 
-		if(node==null) return;
-		this.forEachPost(node.getLeft(), consumer);
-		this.forEachPost(node.getRight(), consumer);
-		consumer.accept(node.getValue());
+	/**
+	 * Loops through the BianarySearchTree
+	 * @param consumer The function to perform on each node
+	 */
+	public void forEachPre(final Consumer<E> consumer){
+		BianarySearchTree.forEachPre(this.root, consumer);
 	}
 
-	private void forEachPre(final Node<E> node, final Consumer<E> consumer){
+	/**
+	 * Returns the height of the tree<br>
+	 * The deepest node's depth plus 1
+	 *
+	 * @return The height of the tree
+	 * @see    tree.Node#height()
+	 */
+	@SuppressWarnings("null")
+	public long height(){
+		if(this.root==null)return 0;
+		return this.root.height();
+	}
 
-		if(node==null)return;
-		consumer.accept(node.getValue());
-		this.forEachPre(node.getLeft(), consumer);
-		this.forEachPre(node.getRight(), consumer);
+	/**
+	 * Inserts the value in the appropriate spot in the tree
+	 *
+	 * @param value
+	 *                  The value to insert
+	 * @see         tree.Node#insert(java.lang.Comparable)
+	 */
+	@SuppressWarnings("null")
+	public void insert(@NonNull
+		final
+		E value){
+		if(this.root==null) this.root=new Node<>(value);
+		this.root.insert(value);
+	}
+
+	/**
+	 * Tests if the node is a valid BianarySearchTree
+	 *
+	 * @return {@code true} if valid, {@code false} if invalid
+	 */
+	public boolean isBianarySearchTree(){
+		return BianarySearchTree.isBianarySearchTree(this.root);
+	}
+
+	/**
+	 * Removes the given value
+	 *
+	 * @param  value
+	 *                   The value to remove
+	 * @return       {@code true} if it exists and was removed {@code false} if it is not found
+	 * @see          tree.Node#remove(java.lang.Comparable)
+	 */
+	@SuppressWarnings("null")
+	public boolean remove(@NonNull
+		final
+		E value){
+		if(this.root==null) return false;
+		return this.root.remove(value);
 	}
 }
 
