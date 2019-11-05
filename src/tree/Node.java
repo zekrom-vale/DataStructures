1d6f16b444af842898ee1f32770ddd8b1990f25e
zekrom-vale <sgraven66@gmail.com>
2019-11-05 08:45:10 -0600

    Added comments and getters/setters

--- src/tree/Node.java
+++ src/tree/Node.java
@@ -1,53 +1,182 @@
 package tree;
 
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
+/**
+ * @author     Zekrom
+ *
+ * @param  <E>
+ *                 The type of the node
+ */
 @SuppressWarnings("hiding")
-public class Node<@Nonnull E extends Comparable<E>>{
-	@Nonnull
-	E value;
-	
-	@Nullable
-	Node<E> right;
-	
+public class Node <@NonNull
+E extends Comparable<E>> implements Comparable<Node<E>>{
 	@Nullable
 	Node<E> left;
-	
-	public Node(Node<E> left, E value, Node<E> right){
+
+	@Nullable
+	Node<E> right;
+
+	@NonNull
+	E value;
+
+
+	/**
+	 * Default Constructor
+	 *
+	 * @param value
+	 *                  The value of the node
+	 */
+	public Node(final E value){
+		this(null, value, null);
+	}
+
+	/**
+	 * Alternate Constructor
+	 *
+	 * @param left
+	 *                  The left Node
+	 *
+	 * @param value
+	 *                  The value of the node
+	 * @param right
+	 *                  The right Node
+	 */
+	public Node(final Node<E> left, final E value, final Node<E> right){
 		this.left=left;
 		this.value=value;
 		this.right=right;
 	}
-	
-	public Node(E value){
-		this(null, value, null);
+
+	@Override
+	public int compareTo(final Node<@NonNull
+		E> node){
+		return this.value.compareTo(node.value);
 	}
-	
-	public Node(Node<E> left, E value){
-		this(left, value, null);
+
+
+	/**
+	 * Counts the amount of leaves in the tree<br>
+	 * The nodes that do not have children
+	 *
+	 * @return the number of leaves
+	 */
+	@SuppressWarnings("null")
+	public long countLeaves(){
+		if(this.right!=null){
+			if(this.left!=null){
+				return this.left.countLeaves()+this.right.countLeaves();
+			}
+			return this.right.countLeaves();
+		}
+		if(this.left!=null){
+			return this.left.countLeaves();
+		}
+		return 1;
 	}
-	
-	public Node(E value, Node<E> right){
-		this(null, value, right);
+
+	/**
+	 * Counts the amount of internal nodes in the tree<br>
+	 * The nodes that do have children
+	 *
+	 * @return the number of internal nodes
+	 */
+	@SuppressWarnings("null")
+	public long countNodes(){
+
+		if(this.right!=null){
+			if(this.left!=null){
+				return this.left.countNodes()+1+this.right.countNodes();
+			}
+			return this.right.countNodes()+1;
+		}
+		if(this.left!=null){
+			return this.left.countNodes()+1;
+		}
+		return 0;
 	}
-	
-	protected void insert(E value){
+
+	/**
+	 * @return the left
+	 */
+	public Node<E> getLeft(){
+		return this.left;
+	}
+
+	/**
+	 * @return the right
+	 */
+	public Node<E> getRight(){
+		return this.right;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public E getValue(){
+		return this.value;
+	}
+
+	/**
+	 * Returns the height of the tree<br>
+	 * The deepest node's depth plus 1
+	 *
+	 * @return The height of the tree
+	 */
+	@SuppressWarnings("null")
+	public long height(){
+		if(this.right==null){
+			if(this.left==null)return 1;
+			return this.left.height()+1;
+		}
+		if(this.left==null)return this.right.height()+1;
+		return Math.max(this.left.height(), this.right.height())+1;
+	}
+
+	/**
+	 * Inserts the value in the appropriate spot in the tree
+	 *
+	 * @param value
+	 *                  The value to add
+	 */
+	@SuppressWarnings("null")
+	public void insert(final E value){
 		if(this.value.compareTo(value)<0){
 			if(this.left==null){
-				this.left=new Node(value);
+				this.left=new Node<>(value);
 				return;
 			}
 			this.left.insert(value);
 			return;
 		}
 		if(this.right==null){
-			this.right=new Node(value);
+			this.right=new Node<>(value);
 			return;
 		}
 		this.right.insert(value);
 	}
-	
-	protected boolean remove(E value){
+
+	/**
+	 * Gets the left most node
+	 *
+	 * @return The minimum node
+	 */
+	@SuppressWarnings("null")
+	public Node<E> leftMost(){
+		if(this.left==null)return this;
+		return this.left.leftMost();
+	}
+
+	/**
+	 * Removes the given value
+	 *
+	 * @param  value
+	 *                   The value to remove
+	 * @return       {@code true} if it exists and was removed {@code false} if it is not found
+	 */
+	@SuppressWarnings("null")
+	public boolean remove(final E value){
 		if(this.value.compareTo(value)<0){
 			if(this.left==null)return false;
 			if(this.left.value.compareTo(value)==0){
@@ -65,60 +194,63 @@ public class Node<@Nonnull E extends Comparable<E>>{
 			return this.right.remove(value);
 		}
 	}
-	
-	protected void removeRight(@Nonnull Node<E> parent){
-		parent.right=this.right;
-		this.leftMost().left=this.left;
-	}
-	
-	protected void removeLeft(@Nonnull Node<E> parent){
+
+	/**
+	 * Removes the current node from the parent
+	 *
+	 * @param parent
+	 *                   The parent of the node
+	 */
+	protected void removeLeft(@NonNull
+		final Node<E> parent){
 		parent.left=this.right;
 		this.leftMost().left=this.left;
 	}
-	
-	public Node<E> leftMost(){
-		if(this.left==null)return this;
-		return this.left.leftMost();
+
+	/**
+	 * Removes the current node from the parent
+	 *
+	 * @param parent
+	 *                   The parent of the node
+	 */
+	protected void removeRight(@NonNull
+		final Node<E> parent){
+		parent.right=this.right;
+		this.leftMost().left=this.left;
 	}
-	
+
+	/**
+	 * Gets the right most node
+	 *
+	 * @return The maximum node
+	 */
+	@SuppressWarnings("null")
 	public Node<E> rightMost(){
-		if(this.right==null)return this;
+		if(this.right==null) return this;
 		return this.right.rightMost();
 	}
-	
-	public long countNodes(){
-		
-		if(this.right!=null){
-			if(this.left!=null){
-				return this.left.countNodes()+1+this.right.countNodes();
-			}
-			return this.right.countNodes()+1;
-		}
-		if(this.left!=null){
-			return this.left.countNodes()+1;
-		}
-		return 0;
+
+	/**
+	 * @param left
+	 *                 the left to set
+	 */
+	public void setLeft(final Node<E> left){
+		this.left=left;
 	}
-	
-	public long countLeaves(){
-		if(this.right!=null){
-			if(this.left!=null){
-				return this.left.countLeaves()+this.right.countLeaves();
-			}
-			return this.right.countLeaves();
-		}
-		if(this.left!=null){
-			return this.left.countLeaves();
-		}
-		return 1;
+
+	/**
+	 * @param right
+	 *                  the right to set
+	 */
+	public void setRight(final Node<E> right){
+		this.right=right;
 	}
-	
-	public long height(){
-		if(this.right==null){
-			if(this.left==null)return 1;
-			return this.left.height()+1;
-		}
-		if(this.left==null)return this.right.height()+1;
-		return Math.max(this.left.height(), this.right.height())+1;
+
+	/**
+	 * @param value
+	 *                  the value to set
+	 */
+	public void setValue(final E value){
+		this.value=value;
 	}
 }
