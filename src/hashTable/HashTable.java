445805e6a1b384dcf3e1bca479fead58d5e9dae1
Shawn <sgraven66@gmail.com>
2019-11-24 16:40:44 -0600

    Added comments

--- src/hashTable/HashTable.java
+++ src/hashTable/HashTable.java
@@ -11,10 +11,18 @@ E, S extends Collection<E>> implements Collection<E>{
 
 	public static final String METHOD="Chaining";
 	private final S[] arr;
+	/**
+	* The method returning a new S object
+	*/
 	@NonNull
 	private final Supplier<S> supplier;
 
-
+	/**
+	* Creates a new HashTable
+	* @param capacity the size of the array to create, should be prime
+	* @param supplier a method that returns a new container
+	* @apinote supplier will be called at the start to get the class of the type
+	*/
 	@SuppressWarnings({"unchecked", "null"})
 	public HashTable(final int capacity, @NonNull
 		final Supplier<@NonNull
@@ -23,6 +31,11 @@ E, S extends Collection<E>> implements Collection<E>{
 		this.arr=(S[])Array.newInstance(supplier.get().getClass(), capacity);
 	}
 
+	/**
+	* Inserts a new element into the hash table
+	* @param value The value to add
+	* @return {@code true} if there is room to insert
+	*/
 	@Override
 	public boolean add(final E value){
 		final int index=this.hashCode(value);
@@ -30,6 +43,9 @@ E, S extends Collection<E>> implements Collection<E>{
 		return this.arr[index].add(value);
 	}
 
+	/**
+	* Checks if the value exists in the hash table
+	*/
 	@Override
 	public boolean exists(final E value){
 		final int index=this.hashCode(value);
@@ -38,13 +54,19 @@ E, S extends Collection<E>> implements Collection<E>{
 	}
 
 	/**
-	 * @param value
-	 * @return
+	 * Hashes the value by calling the respective hashcode method and adjusts for capacity
+	 * @param value The value to hash
+	 * @return The corrected hash value
 	 */
 	private int hashCode(final E value){
 		return Math.abs(value.hashCode())%this.arr.length;
 	}
 
+	/**
+	* Removes the given value
+	* @param value the value to remove
+	* @return {@code true} if the value exists and was deleted
+	*/
 	@Override
 	public @Nullable
 	E remove(final E value){
@@ -53,6 +75,9 @@ E, S extends Collection<E>> implements Collection<E>{
 		return this.arr[index].remove(value);
 	}
 
+	/**
+	* @return A string representation of the hash table
+	*/
 	@Override
 	public String toString(){
 		final StringBuilder builder=new StringBuilder();
