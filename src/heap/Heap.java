33abe60333a092ed43d579184f0afd78267bb543
zekrom-vale <sgraven66@gmail.com>
2019-11-26 07:51:10 -0600

    Fixed removal

--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -1,26 +1,69 @@
 package heap;
 
-import org.eclipse.jdt.annotation.NonNull;
+import java.util.Arrays;
 
-public class Heap <@NonNull
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Class Heap<br>
+ * A loosely sorted max heap based on the array
+ *
+ * @author     Shawn Graven
+ * @date       11/25/19
+ *
+ * @param  <E>
+ *                 The type of the heap, must implement {@link Comparable}
+ */
+public class Heap <@Nullable
 E extends Comparable<E>>{
 
-	final private Object[] arr;
-	private int size=0;
+	/**
+	 * @param index the index of the parent
+	 * @return the index of the left child (right is +1)
+	 */
+	protected static int left(final int index){
+		return 2*index+1;
+	}
+	/**
+	 * @param index the index of the child
+	 * @return the parent index of the child
+	 */
+	protected static int parent(final int index){
+		return (index-1)/2;
+	}
 
 	/**
-	* Creates a new Heap with the given capacity
-	*/
+	 * @param index the index of the parent
+	 * @return the index of the right child (left is -1)
+	 */
+	protected static int right(final int index){
+		return 2*(index+1);
+	}
+
+	final private Object[] arr;
+
+	private int size=0;
+
+
+	/**
+	 * Creates a new Heap with the given capacity
+	 *
+	 * @param capacity
+	 *                     The maximum capacity of the heap
+	 */
 	public Heap(final int capacity){
 		this.arr=new Object[capacity];
 	}
-	
+
 	/**
-	* Adds the value to the heap
-	* @param value The value to add
-	* @return {@code true} if successful 
-	*/
-	public boolean add(final E value){
+	 * Adds the value to the heap
+	 * @param value The value to add
+	 * @return {@code true} if successful
+	 */
+	@SuppressWarnings("null")
+	public boolean add(@NonNull
+		final E value){
 		if(this.arr[0]==null){
 			this.arr[0]=value;
 			this.size=1;
@@ -28,37 +71,36 @@ E extends Comparable<E>>{
 		}
 		//Don't insert if full
 		if(this.size==this.arr.length) return false;
-		//Insert at end and trickel up
+		//Insert at end and trickle up
 		this.arr[this.size]=value;
-		int parent=this.parent(this.size);
+		int parent=Heap.parent(this.size);
 		int child=this.size++;
 		while(this.get(parent).compareTo(this.get(child))<0){
 			this.swap(parent, child);
 			if(parent==0) break;
 			child=parent;
-			parent=this.parent(parent);
+			parent=Heap.parent(child);
 		}
 		return true;
 	}
-	
+
 	/**
-	* @return the depth of the given index
-	*/
+	 * @return the depth of the given index
+	 */
 	protected int depth(final int index){
 		return (int)Math.ceil(Math.log(index+2)/Math.log(2))-1;
 	}
-	
-	
+
 	/**
-	* @return the index from the right of the tree of the given index
-	*/
+	 * @return the index from the right of the tree of the given index
+	 */
 	protected int fromRight(final int index){
 		return index-(int)Math.pow(2, this.depth(index))+1;
 	}
-	
+
 	/**
-	* Returns the casted value of the index
-	*/
+	 * Returns the casted value of the index
+	 */
 	@SuppressWarnings("unchecked")
 	protected E get(final int index) {
 		return (E)this.getRaw(index);
@@ -73,65 +115,78 @@ E extends Comparable<E>>{
 		return index<this.arr.length?this.arr[index]:null;
 	}
 
-	/**
-	* @param index the index of the parent
-	* @return the index of the left child (right is +1)
-	*/
-	protected int left(final int index){
-		return 2*index+1;
-	}
-	
-	/**
-	* @param index the index of the child
-	* @return the parent index of the child
-	*/
-	protected int parent(final int index){
-		return (index-1)/2;
+	public boolean isEmpty(){
+		return this.size==0;
 	}
 
 	/**
-	* Removes the top value in the heap
-	* @return the removed vaule
-	*/
-	public E remove(){
+	 * Removes the top value in the heap
+	 * @return the removed value
+	 */
+	@SuppressWarnings({"null"})
+	public @Nullable
+	E remove(){
+		if(this.size==0) return null;
+		if(this.size==1){
+			final E value=this.get(--this.size);
+			this.arr[0]=null;
+			return value;
+		}
 		final E value=this.get(0);
+		//Insert last value into root
+		this.arr[0]=this.getRaw(this.size-1);
+		this.arr[--this.size]=null;
+		//Trickle down
 		int parent=0;
 		int child=1;
-		while(this.getRaw(child)!=null){
-			if(this.get(child).compareTo(this.get(child+1))>0){
-				//Left Child is greater
-				this.arr[parent]=this.getRaw(child);
-				this.arr[child]=null;
-				parent=child;
+		while(child<this.size){//Loop only if child is within bounds
+			System.out.println(this);
+			if(
+				this.get(child+1)!=null&&
+				this.get(child).compareTo(this.get(child+1))<0
+				){
+				//Right Child is greater (Shift to point to the right child)
+				//Check if it exists
+				if(this.getRaw(++child)==null) return value;
 			}
-			else{
-				//Right Child is greater
-				this.arr[parent]=this.getRaw(child+1);
-				this.arr[child+1]=null;
-				parent=child+1;
+			//Otherwise Left Child is greater (No shift required)
+			if(this.get(child).compareTo(this.get(parent))<=0){
+				//Done swapping
+				return value;
 			}
-			child=this.left(parent);
+			//Swap
+			this.swap(parent, child);
+			//Get the next parent and child
+			parent=child;
+			child=Heap.left(parent);
 		}
-		//TODO fix non complete tree
+		System.out.println(this);
 		return value;
 	}
-	
+
 	/**
-	* @param index the index of the parent
-	* @return the index of the right child (left is -1)
-	*/
-	protected int right(final int index){
-		return 2*(index+1);
+	 * @return the size
+	 */
+	public int size(){
+		return this.size;
 	}
-	
+
 	/**
-	* Swaps the two index values
-	* @param index1 the first index to swap
-	* @param index2 the seacond index to swap
-	*/
+	 * Swaps the two index values
+	 *
+	 * @param index1
+	 *                   the first index to swap
+	 * @param index2
+	 *                   the second index to swap
+	 */
 	protected void swap(final int index1, final int index2){
 		final Object obj=this.getRaw(index1);
 		this.arr[index1]=this.getRaw(index2);
 		this.arr[index2]=obj;
+	}
+
+	@Override
+	public String toString(){
+		return "Heep "+Arrays.toString(this.arr);
 	}
 }
