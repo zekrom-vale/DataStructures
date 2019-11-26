ed171af97c68fe9905424878fd2484db9b3d6317
Shawn <sgraven66@gmail.com>
2019-11-26 17:54:33 -0600

    Added info

--- src/hashTable/Collection.java
+++ src/hashTable/Collection.java
@@ -1,7 +1,14 @@
 package hashTable;
 
 import org.eclipse.jdt.annotation.Nullable;
-
+/**
+ * Interface Collection creates a shared interface that can be used with the HashTable
+ *
+ * @author     Shawn Graven
+ * @date       11/23/19
+ *
+ * @param <E> The type of the value it holds
+*/
 public interface Collection <E>{
 	boolean add(E value);
 
