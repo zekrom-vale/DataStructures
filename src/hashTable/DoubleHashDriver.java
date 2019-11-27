28efa9757a48bdc2ea2429c395e8cb6cf0424171
Shawn <sgraven66@gmail.com>
2019-11-26 18:10:30 -0600

    Added comments

--- src/hashTable/DoubleHashDriver.java
+++ src/hashTable/DoubleHashDriver.java
@@ -4,6 +4,14 @@ import org.eclipse.jdt.annotation.NonNull;
 
 import tree.BianarySearchTree;
 
+/**
+ * Class DoubleHashDriver tests the stacking feature of the HashTable by creating HashTables of HashTables of BianarySearchTrees<br>
+ * This is a little to convoluted for most users to use, but it's posible
+ *
+ * @author     Shawn Graven
+ * @date       11/26/19
+ *
+*/
 public class DoubleHashDriver{
 
 	public static void main(final String[] args){
