63fde8618a2ecb212ddf62fef3a17db89aa980c9
zekrom-vale <sgraven66@gmail.com>
2019-12-15 19:23:37 -0600

    Aded comment back from revert

--- src/bfs_dfs_and_mst_unweighted_undirected/Wrapper.java
+++ src/bfs_dfs_and_mst_unweighted_undirected/Wrapper.java
@@ -9,6 +9,13 @@ import org.eclipse.jdt.annotation.Nullable;
 import linkedList.LinkedList;
 import linkedList.Node;
 
+/**
+ * Simple wrapper to hold the vertex and linked vertexes for the adjacency list
+ *
+ * @author Shawn Graven
+ * @date   12/15/19
+ */
+
 public class Wrapper{
 
 	@Nullable
